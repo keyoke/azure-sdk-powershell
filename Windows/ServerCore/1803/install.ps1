441e07207cb8a50a53d424cf59c6e13319a4d9f4
Gareth Emslie <gareth@soft-cor.es>
2019-04-04 17:18:34 +0200

    disable progress for ps

--- Windows/ServerCore/1803/install.ps1
+++ Windows/ServerCore/1803/install.ps1
@@ -1,3 +1,4 @@
+$progressPreference = 'silentlyContinue' 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az -AllowClobber -Force
 Enable-AzureRmAlias -Scope LocalMachine
